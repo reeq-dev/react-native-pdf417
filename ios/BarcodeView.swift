11e7c1506ec48d8fba699dfe1d1c38a7fe252456
Ganas Alexander <nooolie@gmail.com>
2024-09-04 13:03:21 +0300

    DEV: Moving the barcode generation to a background thread and updating the UI on the main thread

--- ios/BarcodeView.swift
+++ ios/BarcodeView.swift
@@ -45,18 +45,20 @@ final class BarcodeView: UIView {
         
         let data = (self.text as String).data(using: String.Encoding.ascii)
         
-        DispatchQueue.main.async {
+        DispatchQueue.global(qos: .userInitiated).async {
             if let filter = CIFilter(name: "CIPDF417BarcodeGenerator") {
-                    filter.setValue(data, forKey: "inputMessage")
-                    let transform = CGAffineTransform(scaleX: 3, y: 3)
-
-                    if let output = filter.outputImage?.transformed(by: transform) {
-                        let image = UIImage(ciImage: output)
+                filter.setValue(data, forKey: "inputMessage")
+                let transform = CGAffineTransform(scaleX: 3, y: 3)
+                
+                if let output = filter.outputImage?.transformed(by: transform) {
+                    let image = UIImage(ciImage: output)
+                    
+                    DispatchQueue.main.async {
                         let imageView = UIImageView(image: image)
-                        
                         imageView.frame = CGRect(x: 0, y: 0, width: self.bounds.width, height: self.bounds.height)
                         imageView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
                         self.addSubview(imageView)
+                    }
                 }
             }
         }
